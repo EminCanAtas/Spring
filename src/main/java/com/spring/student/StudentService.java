f872480d0925a6abe77559093d84f0c8f848b73b
EminCanAtas <ghostreconjaga@gmail.com>
2022-10-02 12:56:12 +0300

    feature: Student update request added

--- src/main/java/com/spring/student/StudentService.java
+++ src/main/java/com/spring/student/StudentService.java
@@ -3,7 +3,9 @@ package com.spring.student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -39,5 +41,28 @@ public class StudentService {
         }
 
         studentRepository.deleteById(studentId);
+    }
+
+    @Transactional
+    public void updateStudent(Long studentId, String studentName, String studentEmail) {
+        Student student = studentRepository.findById(studentId)
+                .orElseThrow(() -> new IllegalStateException("Student with id " + studentId + " does not exists"));
+
+        if (studentName != null && studentName.length() > 0 && !Objects.equals(student.getName(), studentName)) {
+            student.setName(studentName);
+        }
+
+        if (studentEmail != null && studentEmail.length() > 0 && !Objects.equals(student.getEmail(), studentEmail)) {
+            Optional<Student> studentByEmail = studentRepository
+                    .findStudentByEmail(student.getEmail());
+
+            if (studentByEmail.isPresent()) {
+                throw new IllegalStateException("Email is taken");
+            }
+
+            student.setEmail(studentEmail);
+        }
+
+        studentRepository.u
     }
 }
